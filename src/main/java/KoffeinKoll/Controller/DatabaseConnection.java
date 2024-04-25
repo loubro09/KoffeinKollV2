595b1873fa98ac2ff9a1d35e1e0f6009e7425a16
AlanahCoolman <alanah.emily.coleman@gmail.com>
2024-04-25 13:52:14 +0200

    Caffeinetracker

--- src/main/java/KoffeinKoll/Controller/DatabaseConnection.java
+++ src/main/java/KoffeinKoll/Controller/DatabaseConnection.java
@@ -12,7 +12,7 @@ public class DatabaseConnection {
     private String password;
     private static DatabaseConnection instance;
 
-    private DatabaseConnection() {
+    DatabaseConnection() {
         loadConfiguration();
     }
 
