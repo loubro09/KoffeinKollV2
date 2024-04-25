3fd311543f81879d3b5130fa1c949da42e96de55
AlanahCoolman <alanah.emily.coleman@gmail.com>
2024-04-25 13:51:07 +0200

    Create User.java

---
+++ src/main/java/KoffeinKoll/Model/User.java
@@ -0,0 +1,104 @@
+package KoffeinKoll.Model;
+
+import KoffeinKoll.Controller.DatabaseConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class User {
+    private int id;
+    private String username;
+    private double weight;
+    private DatabaseConnection databaseConnection;
+
+    public User() {
+        this.id = id;
+        this.username = username;
+        this.weight = weight;
+        this.databaseConnection = DatabaseConnection.getInstance();
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public double getWeight() {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        double weight = 0;
+
+        try {
+            connection = databaseConnection.getConnection();
+            preparedStatement = connection.prepareStatement("SELECT weight FROM users WHERE username = ?");
+            preparedStatement.setString(1, username);
+            resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                weight = resultSet.getDouble("weight");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            // Stäng resurser
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return weight;
+    }
+
+    public int getId() {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        int id = 0;
+
+        try {
+            connection = databaseConnection.getConnection();
+            preparedStatement = connection.prepareStatement("SELECT user_id FROM users WHERE username = ?");
+            preparedStatement.setString(1, username);
+            resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                id = resultSet.getInt("user_id");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            // Stäng resurser
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return id;
+    }
+
+
+}
