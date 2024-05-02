999bf321d367bbf38a0ee660627e350367d35273
discodisco17 <nordenswanida@gmail.com>
2024-05-02 11:29:07 +0200

    ny klass AlgortihmController
    behövde börja om med ny branch

--- src/main/java/KoffeinKoll/Controller/AlgorithmController.java
+++ src/main/java/KoffeinKoll/Controller/AlgorithmController.java
@@ -1,29 +1,40 @@
 package KoffeinKoll.Controller;
 
 
+import KoffeinKoll.View.BeverageMenuPage;
+
+import java.util.Map;
+
 public class AlgorithmController {
+    private final int bevaregeId;
+    private final double beverageAmount;
+    private DatabaseConnection databaseConnection;
     private double halfLife; // Half-life in hours
 
-    public AlgorithmController(double halfLife) {
-        this.halfLife = halfLife;
+    public AlgorithmController(int bevaregeId, double beverageAmount) {
+        this.bevaregeId = bevaregeId;
+        this.beverageAmount = beverageAmount;
     }
 
     public double calculateTime(double cF, double cO) {
         return halfLife * Math.log(cF / cO) / Math.log(2);
     }
 
+
     public static void main(String[] args) {
+        int beverageId = 1;
         double halfLife = 5.7; // hours
         double cF = 1; // mg
 
 
-       /* double cO = mängd dryck * koffein-koncentration
+        //for (beverageId){
 
-        AlgorithmController controller = new AlgorithmController(halfLife);
+       /* double cO = (beverageAmount * koffein-koncentration)
+
+        AlgorithmController controller = new AlgorithmController(beverageId, beverageAmount);
         double time = controller.calculateTime(cF, cO);
-
         System.out.println("Time required: " + time + " hours");
-        
-        */
+
+        } */
     }
 }
