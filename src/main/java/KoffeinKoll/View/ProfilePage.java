61e97f6cbca1f38ce2de428aa665cd1efe3cae91
loubro09 <louis.r.brown@hotmail.com>
2024-05-02 19:23:55 +0200

    Update ProfilePage.java

--- src/main/java/KoffeinKoll/View/ProfilePage.java
+++ src/main/java/KoffeinKoll/View/ProfilePage.java
@@ -9,11 +9,13 @@ import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
-
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-
+/**
+ * ProfilePage class represents the user profile page where users can update their information.
+ * It extends A_Page and implements methods to initialize UI components, set event handlers, and set the scene.
+ */
 public class ProfilePage extends A_Page {
     private Label lbl_newHabit;
     private Label lbl_newWeight;
@@ -29,7 +31,10 @@ public class ProfilePage extends A_Page {
     private UserController userController;
 
 
-
+    /**
+     * Initializes the UI components.
+     * @author Louis Brown
+     */
     @Override
     public void initializeUI() {
         setComponents();
@@ -38,6 +43,10 @@ public class ProfilePage extends A_Page {
         this.userController = UserController.getInstance();
     }
 
+    /**
+     * Sets up UI components.
+     * @author Louis Brown
+     */
     @Override
     public void setComponents() {
         setLabels();
@@ -47,6 +56,10 @@ public class ProfilePage extends A_Page {
         setDatePicker();
     }
 
+    /**
+     * Sets event handlers for buttons.
+     * @author                                                                                          //AUTHOR
+     */
     @Override
     public void setEvents() {
         btn_goHome.setOnAction(event -> {
@@ -108,6 +121,10 @@ public class ProfilePage extends A_Page {
         });
     }
 
+    /**
+     * Sets up the scene layout.
+     * @author                                                                                          //AUTHOR
+     */
     @Override
     public void setScene() {
         GridPane gridPane = new GridPane();
@@ -135,17 +152,29 @@ public class ProfilePage extends A_Page {
         borderPane.setBottom(buttonBox);
     }
 
+    /**
+     * Sets up labels.
+     * @author                                                                                          //AUTHOR
+     */
     private void setLabels() {
         lbl_newHabit = setLabelStyle("New Habit:");
         lbl_newWeight = setLabelStyle("New Weight:");
         lbl_newDateOfBirth = setLabelStyle("New Date of Birth:");
     }
 
+    /**
+     * Sets up text fields.
+     * @author                                                                                          //AUTHOR
+     */
     private void setTextfields() {
         tf_newWeight = setTextField();
         tf_newWeight.setPromptText("Weight (kg)");
     }
 
+    /**
+     * Sets up buttons.
+     * @author                                                                                          //AUTHOR
+     */
     private void setButtons() {
         btn_goHome = new JFXButton("Back");
         btn_goHome.setStyle(setButtonStyle());
@@ -154,6 +183,10 @@ public class ProfilePage extends A_Page {
         btn_save.setStyle(setButtonStyle());
     }
 
+    /**
+     * Sets up radio buttons.
+     * @author Louis Brown
+     */
     private void setRadioButton() {
         toggleGroup = new ToggleGroup();
 
@@ -168,32 +201,52 @@ public class ProfilePage extends A_Page {
         rb_option3.setToggleGroup(toggleGroup);
     }
 
+
+    /**
+     * Retrieves the selected habit value from radio buttons.
+     * @return The selected habit value.
+     * @author Louis Brown
+     */
     private String habitValue() {
         if (toggleGroup.getSelectedToggle() != null) {
             RadioButton selectedRadioButton = (RadioButton) toggleGroup.getSelectedToggle();
             return selectedRadioButton.getText();
         }
-        // Return a default value if no radio button is selected
         return null;
     }
 
+    /**
+     * Sets up date picker.
+     * @author Alanah Coleman
+     */
     private void setDatePicker() {
         datePicker = new DatePicker(); // Skapa DatePicker-instans
         datePicker.setPromptText("Select Date of Birth"); // Användarinformation
     }
 
+    /**
+     * Checks if the chosen date is at least fifteen years ago.
+     * @param chosenDate The chosen date.
+     * @return True if the date is at least fifteen years ago, false otherwise.
+     * @author Louis Brown
+     */
     private boolean isAtLeastFifteenYearsAgo(LocalDate chosenDate) {
-        // Get the current date
+        //Get the current date
         LocalDate currentDate = LocalDate.now();
 
-        // Calculate the date 15 years ago
+        //Calculate the date 15 years ago
         LocalDate fifteenYearsAgo = currentDate.minusYears(15);
 
-        // Check if the chosen date is at least 15 years ago
+        //Check if the chosen date is at least 15 years ago
         return chosenDate.isBefore(fifteenYearsAgo) || chosenDate.isEqual(fifteenYearsAgo);
     }
 
+    /**
+     * Returns to the home page.
+     * @author                                                                                          //AUTHOR
+     */
     private void goBack() {
         changePage(new HomePage());
     }
 }
+
